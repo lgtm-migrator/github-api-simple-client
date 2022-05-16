aca6ab0a6c39d7b214895cabb0b338b2b8246194
Cristian Penteado <cristianpenteado@protonmail.ch>
2022-05-16 09:30:03 -0300

    add table to display data

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,56 +1,48 @@
 <template>
-<div>
-  <div 
-    v-for="(item, key) in data" 
-    :key="key"
-  >
-      <label> 
-        {{ key.toUpperCase() }}:
-      </label>
-      <br>
-      <img 
-        v-if="key === 'avatar_url'" 
-        width="30" 
-        :src="item" 
-      >
-      <h4 
-        v-else
-      >
-      {{ item }}
-      </h4>
-  </div>
-</div>
+  <table border="1">
+    <tr v-for="(item, key) in data" :key="key">
+      <td>{{ key.toUpperCase() }}:</td>
+      <td v-if="key === 'avatar_url'"> <img  width="30" :src="item" /></td>
+      <td v-else>
+        <h4>
+        {{ item }}
+        </h4>
+      </td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import { emitter } from '../main.js'
-import axios from 'axios'
+import { emitter } from "../main.js";
+import axios from "axios";
 
 export default {
-  name: 'Content',
-  data: function(){
+  name: "Content",
+  data: function () {
     return {
       data: {},
       issueData: [],
-    }
+    };
   },
   methods: {
-    getData(url){
-      axios.get(url)
-      .then(res => {
-        if(res.data[0] === undefined) {
-          this.data = res.data
+    getData(url) {
+      axios.get(url).then((res) => {
+        if (res.data[0] === undefined) {
+          this.data = res.data;
         } else {
-          this.data = res.data[0]
+          this.data = res.data[0];
         }
-      })
-    }
+      });
+    },
   },
   mounted() {
-    emitter.on('change-data-option', value => this.getData(value))
+    emitter.on("change-data-option", (value) => this.getData(value));
   },
-}
+};
 </script>
 
 <style >
+table {
+  width: 100%;
+}
 </style>
